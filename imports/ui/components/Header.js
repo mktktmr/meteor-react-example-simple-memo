c37ea8b8b315019265df333358e2cc9b348ecb8c
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-05-28 14:23:42 +0900

    change header

--- imports/ui/components/Header.js
+++ imports/ui/components/Header.js
@@ -4,8 +4,9 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="header">
-        <h1>Simple Memo</h1>
+        <h1>Simple Memo on Heroku</h1>
       </header>
     );
   }
 }
+
